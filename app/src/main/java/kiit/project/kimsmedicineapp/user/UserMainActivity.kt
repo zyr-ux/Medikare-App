6b171dcef3048b05b33e54105e310682654e6613
zyr-ux <siddharthasxi2003@gmail.com>
2025-03-27 01:15:02 +0530

    minor thing

--- app/src/main/java/kiit/project/kimsmedicineapp/user/UserMainActivity.kt
+++ app/src/main/java/kiit/project/kimsmedicineapp/user/UserMainActivity.kt
@@ -19,8 +19,6 @@ import android.provider.Settings
 import android.util.Log
 import android.view.MotionEvent
 import android.view.View
-import android.view.animation.AccelerateDecelerateInterpolator
-import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
